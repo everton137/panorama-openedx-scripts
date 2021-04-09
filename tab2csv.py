c6130e3ed06d82bf39e605f09ab4a25fda6c7ace
Andrés González <andres@aulasneo.com>
2021-04-09 14:03:56 -0300

    Open csv in universal mode to avoid _csv.Error: new-line character seen in unquoted field

--- tab2csv.py
+++ tab2csv.py
@@ -4,7 +4,7 @@ import csv
 import sys
 
 
-tab_in = csv.reader(sys.stdin, dialect=csv.excel_tab)
+tab_in = csv.reader(open(sys.stdin, mode='rU'), dialect=csv.excel_tab)
 comma_out = csv.writer(sys.stdout, delimiter=',', doublequote=False, escapechar='\\')
 
 for row in tab_in:
